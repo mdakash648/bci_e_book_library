9c0a768f0d3bfb4c2291a0fe6ffd2f1cb3fc7380
mdakash648 <akashnazmul648@gmail.com>
2025-08-02 12:00:35 +0600

    Initial commit
    Generated by react-native@0.80.2

---
+++ android/app/src/main/java/com/bci_e_book_library/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.bci_e_book_library
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "BCI_E_Book_library"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
